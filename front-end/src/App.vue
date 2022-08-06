fec7994436a8bd9938fefffe403940214f6cdcf9
Rarysson Guilherme <r_guilherme12@hotmail.com>
2022-08-06 00:28:56 -0300

    Add base layout

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1 class="text-3xl font-bold underline">App</h1>
+  <div class="flex flex-col h-full">
+    <h1 class="text-center font-bold text-3xl">Golden Eye</h1>
+
+    <main
+      class="flex-auto w-full max-w-6xl mx-auto bg-white mt-7 p-4 rounded-lg"
+    >
+      <h2 class="font-bold text-lg">Transactions</h2>
+    </main>
+  </div>
 </template>
