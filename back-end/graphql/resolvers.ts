e0053b6357d4da6090434504eaba37373b3f13c0
Rarysson Guilherme <r_guilherme12@hotmail.com>
2022-08-07 23:45:45 -0300

    Add GraphQL API types

--- back-end/graphql/resolvers.ts
+++ back-end/graphql/resolvers.ts
@@ -1,1 +1,17 @@
-export const resolvers = {};
+const { GraphQLScalarType } = require("graphql");
+
+const dateScalar = new GraphQLScalarType({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value: Date) {
+    return value.toISOString();
+  },
+  parseValue(value: string) {
+    return new Date(value);
+  }
+});
+
+export const resolvers = {
+  Date: dateScalar,
+  Query: {}
+};
--- back-end/graphql/typeDefs.ts
+++ back-end/graphql/typeDefs.ts
@@ -1,3 +1,46 @@
 import { gql } from "apollo-server";
 
-export const typeDefs = gql``;
+export const typeDefs = gql`
+  scalar Date
+
+  type Account {
+    id: String!
+    name: String!
+  }
+
+  type Category {
+    id: String!
+    name: String!
+    color: String
+  }
+
+  type Money {
+    amount: Float!
+    currency: String!
+  }
+
+  type Transaction {
+    id: String!
+    reference: String
+    date: Date!
+    category: Category!
+    money: Money!
+  }
+
+  input TransactionFilter {
+    accountId: String
+    startingMonth: Date
+    endingMonth: Date
+  }
+
+  input TransactionInput {
+    first: Int!
+    offset: Int
+    filters: TransactionFilter
+  }
+
+  type Query {
+    accounts: [Account!]!
+    transactions(input: TransactionInput): [Transaction!]!
+  }
+`;
