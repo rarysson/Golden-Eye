6fe2f30d5a26134f1261892a4b34380e75be4645
Rarysson Guilherme <r_guilherme12@hotmail.com>
2022-08-08 11:59:05 -0300

    add query to fetch transactions data

--- back-end/graphql/resolvers.ts
+++ back-end/graphql/resolvers.ts
@@ -1,5 +1,9 @@
 import { Account } from "@prisma/client";
 import { ApolloContext } from "../interfaces/apolloContext";
+import {
+  TransactionsQueryInput,
+  TransactionsQueryResponse
+} from "../interfaces/transaction";
 import { EmptyObject } from "../types/internals";
 import { dateScalar } from "./scalarTypes";
 
@@ -12,6 +16,35 @@ export const resolvers = {
       context: ApolloContext
     ): Promise<Account[]> {
       return context.prisma.account.findMany();
+    },
+
+    async transactions(
+      _parent: void,
+      { input }: TransactionsQueryInput,
+      context: ApolloContext
+    ): Promise<TransactionsQueryResponse[]> {
+      const offset = (input.offset ?? 1) - 1;
+      const skipCount = offset * input.first;
+
+      const transactions = await context.prisma.transaction.findMany({
+        take: input.first,
+        skip: skipCount,
+        include: { category: true }
+      });
+
+      return transactions.map((transaction) => ({
+        id: transaction.id,
+        date: transaction.date,
+        reference: transaction.reference,
+        category: {
+          name: transaction.category.name,
+          color: transaction.category.color
+        },
+        money: {
+          amount: transaction.amount,
+          currency: transaction.currency
+        }
+      }));
     }
   }
 };
--- back-end/interfaces/transaction.ts
+++ back-end/interfaces/transaction.ts
@@ -7,3 +7,31 @@ export interface Transaction {
   currency: string;
   date: string;
 }
+
+interface TransactionsFilter {
+  accountId?: string | null;
+  startingMonth?: string | null;
+  endingMonth?: string | null;
+}
+
+export interface TransactionsQueryInput {
+  input: {
+    first: number;
+    offset?: number;
+    filters?: TransactionsFilter | null;
+  };
+}
+
+export interface TransactionsQueryResponse {
+  id: string;
+  reference: string | null;
+  date: Date;
+  category: {
+    name: string;
+    color: string | null;
+  };
+  money: {
+    amount: number;
+    currency: string;
+  };
+}
